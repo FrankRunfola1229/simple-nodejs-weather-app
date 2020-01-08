b1e2e9c359adc851b8f4603ced6e867b67b86a49
Runfola,Frank <runfola.frank@healthnow.org>
2020-01-08 08:17:39 -0500

    add ejs

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const request = require("request")
+const app = express()
+const apiKey = "96c763e2a91557d9f467f023c481c078"
+const port = process.env.PORT || 3000
+
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+	res.render("index", { weather: null, error: null })
+})
+
+app.post("/", (req, res) => {
+	let city = req.body.city
+	let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+	request(url, (err, response, body) => {
+		if (err) {
+			res.render("index", { weather: null, error: "Error, please try again" })
+		}
+		else {
+			let weather = JSON.parse(body)
+			if (weather.main == undefined) {
+				res.render("index", { weather: null, error: "Error, please try again" })
+			}
+			else {
+				let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`
+				res.render("index", { weather: weatherText, error: null })
+			}
+		}
+	})
+})
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}!`)
+})
