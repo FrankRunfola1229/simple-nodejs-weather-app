ad45682a162e885bb89a3dc77be60ab32b4f4a92
Runfola,Frank <runfola.frank@healthnow.org>
2020-01-08 09:01:47 -0500

    add secret config

--- app.js
+++ app.js
@@ -14,29 +14,29 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
-	res.render("index", { weather: null, error: null })
+   res.render("index", { weather: null, error: null })
 })
 
 app.post("/", (req, res) => {
-	let city = req.body.city
-	let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+   let city = req.body.city
+   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
-	request(url, (err, response, body) => {
-		if (err) {
-			res.render("index", { weather: null, error: "Error, please try again" })
-		}
-		else {
-			let weather = JSON.parse(body)
+   request(url, (err, response, body) => {
+      if (err) {
+         res.render("index", { weather: null, error: "Error, please try again" })
+      }
+      else {
+         let weather = JSON.parse(body)
 
-			if (weather.main == undefined) {
-				res.render("index", { weather: null, error: "Error, please try again" })
-			}
-			else {
-				let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`
-				res.render("index", { weather: weatherText, error: null })
-			}
-		}
-	})
+         if (weather.main == undefined) {
+            res.render("index", { weather: null, error: "Error, please try again" })
+         }
+         else {
+            let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`
+            res.render("index", { weather: weatherText, error: null })
+         }
+      }
+   })
 })
 
 app.listen(port, () => console.log(`listening on port ${port}!`))
