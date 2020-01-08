7ad6cf24c1a6331f60d401e646a5124b13ed9365
Runfola,Frank <runfola.frank@healthnow.org>
2020-01-08 08:40:23 -0500

    add secret config

--- app.js
+++ app.js
@@ -1,38 +1,42 @@
-const express = require('express');
+"use strict"
+const express = require("express")
 const app = express()
-const bodyParser = require('body-parser');
-const request = require('request');
-const apiKey = "96c763e2a91557d9f467f023c481c078";
-const port = process.env.PORT || 3000;
+const bodyParser = require("body-parser")
+const request = require("request")
+const apiKey = process.env.MY_API_KEY
+const port = process.env.PORT || 3000
 
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs')
+// require and load dotenv
+require("dotenv").load()
 
-app.get('/', (req, res) => {
-  res.render('index', {weather: null, error: null});
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+	res.render("index", { weather: null, error: null })
 })
 
-app.post('/', (req, res) => {
-  let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+app.post("/", (req, res) => {
+	let city = req.body.city
+	let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
-  request(url, (err, response, body) => {
-    if(err){
-      res.render('index', {weather: null, error: 'Error, please try again'});
-    }
-    else {
-      let weather = JSON.parse(body)
+	request(url, (err, response, body) => {
+		if (err) {
+			res.render("index", { weather: null, error: "Error, please try again" })
+		}
+		else {
+			let weather = JSON.parse(body)
 
-      if(weather.main == undefined){
-        res.render('index', {weather: null, error: 'Error, please try again'});
-      } 
-      else {
-        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        res.render('index', {weather: weatherText, error: null});
-      }
-    }
-  });
+			if (weather.main == undefined) {
+				res.render("index", { weather: null, error: "Error, please try again" })
+			}
+			else {
+				let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`
+				res.render("index", { weather: weatherText, error: null })
+			}
+		}
+	})
 })
 
 app.listen(port, () => console.log(`listening on port ${port}!`))
